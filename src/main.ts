5e8edb711d785b58b035081c009947cad2c6318f
PavuksWeb <112330889+PavuksWeb@users.noreply.github.com>
2025-05-09 21:04:02 +0300

    Hotfix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:8080',
+  });
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 start().catch((err) => {
